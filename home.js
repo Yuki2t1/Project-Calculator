0a220eb20fb11e62d92ad0138263f8b1fdcb2ec4
Yuki2t1 <faydox2371@gmail.com>
2025-02-22 01:44:37 -0800

    more to come

---
+++ home.js
@@ -0,0 +1,45 @@
+let first_number=0
+let operator=""
+let second_number=0
+
+
+
+//function performs addition
+function add(val1,val2){
+    res=Number(val1)+Number(val2)
+    return res
+}
+//function performs subtraction
+function sub(val1,val2){
+    res=val1-val2
+    return res 
+}
+//function performs mulitiplication
+function multiply(val1,val2){
+    res=val1*val2
+    return res
+}
+
+//function performs division
+function division(val1,val2){
+    res=val1/val2
+    return res
+}
+
+//funciton that performs the needed calculation based upon the operator.
+function operate ( ){
+    let first_number=prompt("give me the first number")
+    let operator=prompt("give me an operator")
+    let second_number=prompt("give me secon number")
+    if (operator==="+"){
+        res=add(first_number,second_number)
+    }else if(operator==="-"){
+        res=sub(first_number,second_number)
+    }else if(operator==="*"){
+        res=multiply(first_number,second_number)
+    }else if(operator==="/"){
+        res=division(first_number,second_number)
+    }return res
+}
+
+
